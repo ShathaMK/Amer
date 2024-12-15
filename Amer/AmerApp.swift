ae0aa6e10f8457c24e3381f6c70dd89a0a4ba6ab
ShathaMK <shathamukhaild@gmail.com>
2024-12-15 10:08:10 +0300

    Initial Commit

---
+++ Amer/AmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmerApp.swift
+//  Amer
+//
+//  Created by Shatha Almukhaild on 14/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
