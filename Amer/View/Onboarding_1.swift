020645912cc15d0b5817d34699add49a9135f643
noora amer <aoora6@gmail.com>
2024-12-16 10:00:21 +0300

    here i added all the animation assets and the code for gif images

---
+++ Amer/View/Onboarding_1.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding_1.swift
+//  Amer
+//
+//  Created by Noori on 15/12/2024.
+//
+
+import SwiftUI
+
+struct Onboarding_1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding_1()
+}
