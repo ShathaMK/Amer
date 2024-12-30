5486490cf16f696d45e9716e55fc514bdcfce5be
noora amer <aoora6@gmail.com>
2024-12-30 05:12:28 +0300

    till now APNs authentication key has been created in the apple dev account and everything is going fine

--- Amer/AmerApp.swift
+++ Amer/AmerApp.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 import Firebase
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
 @main
 struct AmerApp: App {
@@ -26,14 +29,36 @@ struct AmerApp: App {
 
 
 
-// i could use smth else but lets keep on trac with the traditional
+// MARK: - i could use smth else but lets keep on trac with the traditional
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//        
+//        FirebaseApp.configure()
+//        print("FireBase is here to Serve!!!")
+//        return true
+//    }
+//}
+
+
+// MARK: - here i am improving it to able to send and recive from the FireBase
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, error in
+            guard granted else { return }
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
+        
         print("FireBase is here to Serve!!!")
+        
         return true
     }
 }
-
